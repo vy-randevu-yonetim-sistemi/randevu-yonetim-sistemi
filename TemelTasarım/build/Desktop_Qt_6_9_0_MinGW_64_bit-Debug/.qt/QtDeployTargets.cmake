9ee6804ee953accf645b41130cdfcd246e8461c1
Kaan <kaanrkaraman@gmail.com>
2025-04-22 10:43:18 +0300

    .Gitignore eklendi ve build klasörü silindi

--- TemelTasarım/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_VeriYapilari_FILE C:/Users/Hasan/Documents/VeriYapilari/build/Desktop_Qt_6_9_0_MinGW_64_bit-Debug/VeriYapilari.exe)
-set(__QT_DEPLOY_TARGET_VeriYapilari_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_VeriYapilari_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Sql.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
